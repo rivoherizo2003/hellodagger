d002cfab0bf07ebbf439f858cccbd63e2f50bf16
apple <apple@apples-MacBook-Pro.local>
2024-05-21 15:45:12 +0400

    edited: call sdk java dagger

--- src/main/java/com/javadagger/App.java
+++ src/main/java/com/javadagger/App.java
@@ -1,5 +1,7 @@
 package com.javadagger;
 
+import javax.management.Query;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,22 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        QueryBuilder queryBuilder = new QueryBuilderImpl();
+        Query query = queryBuilder
+                .container()
+                .from("openjdk:11")
+                .withExec("java", "--version")
+                .build();
+
+        CreateGraphqlQuery createGraphqlQuery = new CreateGraphqlQueryImpl();
+        String graphqlQuery = createGraphqlQuery.create(query);
+
+        ClientSdkDagger clientSdkDagger = new ClientSdkDagger();
+        clientSdkDagger
+                .createClient(String.format("http://127.0.0.1:{}/query"))
+                .withToken("081b3db6-1102-4088-843e-758fdccfbe52")
+                .createRequestHttpGet(graphqlQuery)
+                .execute();
+
     }
 }
